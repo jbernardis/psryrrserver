0a9eee71fbfe1303df6997051a32cb041a02cc4f
jeff bernardis <jeff.bernardis@gmail.com>
2022-11-16 19:49:32 -0500

    preliminary implementation of GM east

--- districts/cliff.py
+++ districts/cliff.py
@@ -9,9 +9,10 @@ class Cliff(District):
 		District.__init__(self, parent, name, settings)
 
 		sigNames =  [
+			"C2RA", "C2RB", "C2RC", "C2RD", "C2L",
 			"C4R", "C4LA", "C4LB", "C4LC", "C4LD",
 		]
-		toNames = [ "CSw37", "CSw39", "CSw41" ]
+		toNames = [ "CSw31", "CSw33", "CSw35", "CSw37", "CSw39", "CSw41" ]
 		hsNames = [ "CSw3" ]
 		handswitchNames = [ "CSw3.hand" ]
 
@@ -20,9 +21,17 @@ class Cliff(District):
 			"CC10W":  [ ["CSw41", "N"], ["CSw39", "N"] ],
 			"CC30W":  [ ["CSw41", "N"], ["CSw39", "R"], ["CSw37", "R"] ],
 			"CC31W":  [ ["CSw41", "N"], ["CSw39", "R"], ["CSw37", "N"] ],
+
+			"CG12E":  [ ["CSw31", "R"], ["CSw35", "N"] ],
+			"CG10E":  [ ["CSw31", "R"], ["CSw35", "R"] ],
+			"CC10E":  [ ["CSw31", "N"], ["CSw33", "N"] ],
+			"CC30E":  [ ["CSw31", "N"], ["CSw33", "R"] ],
 		}
 		ix = 0
-		nxButtons = [ "CG21W", "CC10W", "CC30W", "CC31W" ]
+		nxButtons = [
+			"CG21W", "CC10W", "CC30W", "CC31W",
+			"CG12E", "CG10E", "CC10E", "CC30E",
+		]
 
 		ix = self.AddOutputs(sigNames, SignalOutput, District.signal, ix)
 		ix = self.AddOutputs(nxButtons, NXButtonOutput, District.nxbutton, ix)
@@ -31,7 +40,7 @@ class Cliff(District):
 		for n in nxButtons:
 			self.SetNXButtonPulseLen(n, 2)
 
-		blockNames = [ "G21", "C10", "C30", "C31", "COSGMW" ]
+		blockNames = [ "G21", "C10", "C30", "C31", "COSGMW", "G10", "G12", "C20", "COSGME" ]
 
 		ix = 0
 		ix = self.AddInputs(blockNames, BlockInput, District.block, ix)
